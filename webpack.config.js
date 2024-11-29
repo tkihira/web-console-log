53361e674d1e39db4d0ad91716c83f035083e42b
Takuo Kihira <takuo.kihira@gmail.com>
2024-11-29 20:30:33 +0900

    put plugin outside as npm module

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const DevToolsIgnoreListInlinePlugin = require('devtools-ignore-list-inline-webpack-plugin');
+
 const main = {
     mode: 'production',
     entry: {
@@ -9,6 +11,7 @@ const main = {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [new DevToolsIgnoreListInlinePlugin({ isTarget: (filename => filename.endsWith("web-console-log.js")) })]
 };
 
 module.exports = [main];
